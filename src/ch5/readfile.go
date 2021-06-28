bebafdc0d8ebf07604ee144342a9204345165a6e
csucoderlee <csucoderlee@foxmail.com>
2021-06-28 23:12:59 +0800

    读取文本文件

---
+++ src/ch5/readfile.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	exPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(" os getwd error")
+	}
+
+	//open不是在当前路径下查找, 而是项目的根目录
+	fmt.Println(exPath)
+
+	file, err := os.Open(exPath + "/src/ch5/data.txt")
+
+	if err != nil {
+		log.Fatal(" open file error", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Fatal(" close file error")
+	}
+
+	if scanner.Err() != nil {
+		log.Fatal(" scanner file error,", scanner.Err())
+	}
+}
