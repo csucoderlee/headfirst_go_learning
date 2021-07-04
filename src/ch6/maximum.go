2a6c701611d018f2e117981854e25034618eb6e5
csucoderlee <csucoderlee@foxmail.com>
2021-07-04 23:38:16 +0800

    使用可变长参数的函数

--- src/ch6/maximum.go
+++ src/ch6/maximum.go
@@ -1,6 +1,14 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println(inRange(1, 10, 1, 2, 3, 4, 5, 11))
+}
 
 func maximum(numbers ...float64) float64 {
 
@@ -13,4 +21,16 @@ func maximum(numbers ...float64) float64 {
 	}
 
 	return max
+}
+
+func inRange(min int, max int, numbers ...int) []int {
+
+	var result []int
+
+	for _, number := range numbers {
+		if number > min && number < max {
+			result = append(result, number)
+		}
+	}
+	return result
 }
