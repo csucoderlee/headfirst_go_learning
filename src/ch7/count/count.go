c28da066e136f455eb66ce0b8749aabbd56759ac
csucoderlee <csucoderlee@foxmail.com>
2021-07-13 22:23:50 +0800

    使用map实现投票程序

--- src/ch7/count/count.go
+++ src/ch7/count/count.go
@@ -8,6 +8,12 @@ import (
 
 func main() {
 
+	countWithSlice()
+
+	countWithMap()
+}
+
+func countWithSlice() {
 	lines, err := datafile.GetStrings("votes.txt")
 
 	if err != nil {
@@ -34,5 +40,22 @@ func main() {
 
 	for i, name := range names {
 		fmt.Printf("%s:%d\n", name, counts[i])
+	}
+}
+
+func countWithMap() {
+	lines, err := datafile.GetStrings("votes.txt")
+
+	if err != nil {
+		log.Fatal("datafile GetStrings error", err)
+	}
+
+	names := make(map[string]int)
+	for _, line := range lines {
+		names[line]++
+	}
+
+	for s, i := range names {
+		fmt.Printf("%s : %d\n", s, i)
 	}
 }
